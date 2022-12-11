a5156da5c89ece209ec200157732eff9c3825746
Vivian Zitek <vivian.zitek@gmail.com>
2022-12-10 23:30:31 -0700

    Add StackNoParams method.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 ﻿// StackNoParams
-void StackNoParams() {
+void StackNoParams(int toCreate) {
 
-	
+	double[] valueArray = new double[toCreate];
+	for (int i=0; i < toCreate; i++) {
+		ValueType newValue = new ValueType();
+		valueArray[i] = newValue.Value;
+	}
+
+	foreach (double dbl in valueArray) {Console.WriteLine(dbl);}
 }
 
+StackNoParams(5);
 
 // // Stack Overflow
 // void StackOver() {StackOver();}
